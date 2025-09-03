a6d58ae40d2663ebdf6a94cd2b566d9f2eb077b6
mahmoud <mahmoudelfeky068@gmail.com>
2025-09-03 22:15:52 +0300

    login-feature | init login

--- CrowdFunding/urls.py
+++ CrowdFunding/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('oauth/', include('allauth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('forgotpassord/', include('accounts.urls')),
 ]
